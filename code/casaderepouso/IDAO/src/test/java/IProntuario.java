55d18064ce266d0b4874e207872d9313683a1a99
Fellipe Wallenberg Cardoso Gregorio <fellipewallenberg1@gmail.com>
2023-10-15 15:17:23 -0300

    telas de gestão de medicamento atualização

--- code/casaderepouso/IDAO/src/test/java/IProntuario.java
+++ code/casaderepouso/IDAO/src/test/java/IProntuario.java
@@ -3,7 +3,7 @@ import java.util.Calendar;
 
 public interface IProntuario {
     void setMorador(Morador morador);
-    void setMedico(Medico medico);
+        void setMedico(Medico medico);
     void setHora(Calendar dataHora);
     void setRelatorio(String string);
     Morador getMorador();
