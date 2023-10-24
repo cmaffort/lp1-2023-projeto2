e079e1be0d1e1b47753ce23d59adfe7a67934089
rafael-igor-vilaca <>
2023-10-24 11:26:37 -0300

    Adicionando classes de serviço e classe FuncionarioDAO

---
+++ code/casaderepouso/Service/src/main/java/br/cefetmg/casaderepouso/service/implement/CadastrarFuncionario.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.cefetmg.casaderepouso.service.implement;
+
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+import br.cefetmg.casaderepouso.service.ICadastrarFuncionario;
+import br.cefetmg.casaderepouso.dto.Funcionario;
+import br.cefetmg.casaderepouso.dto.exception.*;
+import br.cefetmg.casaderepouso.DAO.FuncionarioDAO;
+import br.cefetmg.casaderepouso.idao.IFuncionarioDAO;
+
+/**
+ *
+ * @author rafael-igor-vilaca
+ */
+public class CadastrarFuncionario implements ICadastrarFuncionario {
+    private IFuncionarioDAO funcionarioDAO;
+    
+    public CadastrarFuncionario() {
+        funcionarioDAO = new FuncionarioDAO();
+    }
+    
+    @Override
+    public String cadastrar(Funcionario func) throws CadastroException {        
+        funcionarioDAO.inserir(func);
+        return func.getCpf();
+    }
+
+    @Override
+    public String excluir(Funcionario func) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String atualizar(Funcionario func) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+    
+    @Override
+    public List<Funcionario> pesquisar(Funcionario func) throws Exception {
+        List<Funcionario> funcionarios = new ArrayList<Funcionario>();
+        /*if(func.getNome() == null || func.getNome().equals("")) {;
+            funcionarios.addAll(funcionarioDAO.pesquisar(func.getNome()));
+        }*/
+        if(func.getCpf() == null || func.getCpf().equals("")) {
+        }
+        if(func.getDataNasc() == null || func.getDataNasc().toString().equals("")) {
+        }
+        if(func.getEndereco() == null || func.getEndereco().equals("")) {
+        }
+        return null;
+    }
+}
