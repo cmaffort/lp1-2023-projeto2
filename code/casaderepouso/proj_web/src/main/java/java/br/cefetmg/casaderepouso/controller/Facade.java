3272f730ccde9b16ef96522738ba69f13b2019a1
Fellipe Wallenberg Cardoso Gregorio <fellipewallenberg1@gmail.com>
2023-10-30 10:54:50 -0300

    Merge origin/Gestão-de-prontuario into Gestão-de-prontuario
    Conflicts:
    	code/casaderepouso/DAO-MySQL/src/main/java/br/cefetmg/casaderepouso/DAO/connection/DAO.java

--- code/casaderepouso/proj_web/src/main/java/java/br/cefetmg/casaderepouso/controller/Facade.java
+++ code/casaderepouso/proj_web/src/main/java/java/br/cefetmg/casaderepouso/controller/Facade.java
@@ -40,68 +40,17 @@ public class Facade extends HttpServlet {
         if(act.equals("cadastrarMorador")) {
             jsp = CadastrarMorador.execute(request);
         }
-        else if(act.equals("cadastrarFuncionario"))
+        else if(act.equals("cadastrarFuncionario")){
              jsp = CadastrarFuncionario.execute(request);
+        }
+        else if(act.equals("CadastraMedicamento")){
+             jsp = CadastraMedicamento.execute(request);
+        }
+        
+        
+        
         
         RequestDispatcher rd = request.getRequestDispatcher(jsp);
         rd.forward(request, response);
     }
-    
-    
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Facade</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Facade at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
