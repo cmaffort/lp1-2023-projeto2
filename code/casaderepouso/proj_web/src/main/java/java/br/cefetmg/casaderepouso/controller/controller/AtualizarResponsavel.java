ea95e369ed162d01ecbc9f8122452488cc20d9e1
Victor Eduardo dos Reis Prado <104006964+VictorEduReis@users.noreply.github.com>
2023-10-30 18:34:55 -0300

    Add files via upload

---
+++ code/casaderepouso/proj_web/src/main/java/java/br/cefetmg/casaderepouso/controller/controller/AtualizarResponsavel.java
@@ -0,0 +1,61 @@
+package java.br.cefetmg.casaderepouso.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import br.cefetmg.casaderepouso.dto.Responsavel;
+import br.cefetmg.casaderepouso.service.implement.CadastrarResponsavel;
+import br.cefetmg.casaderepouso.service.ICadastrarResponsavel;
+import java.util.ArrayList;
+import java.util.List;;
+
+/**
+ *
+ * @author Aluno
+ */
+@WebServlet(name = "AtualizarResponsavel", urlPatterns = {"/AtualizarResponsavel"})
+public class AtualizarResponsavel extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+   public static String execute(HttpServletRequest request) {
+        String jsp = "";
+     
+        try {
+            String id = request.getParameter("id");
+            CadastrarResponsavel iResponsavel =  new CadastrarResponsavel();
+            Responsavel responsavel = new Responsavel();
+            responsavel.setId(id);
+            String str = iResponsavel.atualizar(responsavel);
+            if (str.equals("true")) {
+                jsp = "/telaGerente.jsp";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            jsp = "";
+        }
+        
+        
+        return jsp;
+    }
+   
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
