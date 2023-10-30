e5ae41d70f76f5cb8358a6bc10f174ab07eb7125
Victor Eduardo dos Reis Prado <104006964+VictorEduReis@users.noreply.github.com>
2023-10-30 08:15:20 -0300

    Update ICadastrarResponsavel.java

--- code/casaderepouso/Service/src/main/java/br/cefetmg/casaderepouso/service/ICadastrarResponsavel.java
+++ code/casaderepouso/Service/src/main/java/br/cefetmg/casaderepouso/service/ICadastrarResponsavel.java
@@ -2,14 +2,15 @@ package br.cefetmg.casaderepouso.service;
 
 import br.cefetmg.casaderepouso.dto.Responsavel;
 import br.cefetmg.casaderepouso.dto.exception.*;
+import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
 
 
 public interface ICadastrarResponsavel {
-    public String cadastrar(Responsavel responsavel) throws CadastroException;
-    public String atualizar(Responsavel responsavel) throws Exception;
-    public String excluir(Responsavel responsavel) throws Exception;
+    public String cadastrar(Responsavel responsavel) throws SQLException, ClassNotFoundException;
+    public String atualizar(Responsavel responsavel) throws SQLException, ClassNotFoundException;
+    public String excluir(Responsavel responsavel) throws SQLException, ClassNotFoundException;
     
     public List<Responsavel> pesquisar(Responsavel responsavel) throws Exception;
 }
